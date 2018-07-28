8a284ea4616ba7ab6a59a3bbecebd684c12f4ed8
milan.zivanov <milan.zivanov@gmail.com>
2018-07-28 15:42:44 +0200

    cleaning scss js html 02

--- js/main.js
+++ js/main.js
@@ -5,45 +5,38 @@ $(document).ready(function () {
       $(".menu_block, .botton").toggleClass("active");
     });
 
-
-	//desktop nav
-	$('.like').on("click", function () {
+    //desktop nav
+    $('.like').on("click", function () {
 
         $(this).addClass('liked');
-        var child = $(this).parent().find(".figure");
+        var child = $(this).parent().find(".counterNum");
         // var text = child.text();
         var num = parseInt(child.text());
         child.text(num+1);
 
-	});
+    });
 
     // scroll to section
-    $("a[href*='#']").bind('click', function(e) {
-            e.preventDefault(); // prevent hard jump, the default behavior
+    $('a[href^="#"]').bind('click', function(e) {
+            e.preventDefault(); 
     
-            var target = $(this).attr("href"); // Set the target as variable
+            var target = $(this).attr("href");
     
-            // perform animated scrolling by getting top-position of target-element and set it as scroll target
             $('html, body').stop().animate({
                 scrollTop: $(target).offset().top
             }, 600, function() {
-                    location.hash = target; //attach the hash (#jumptarget) to the pageurl
+                    location.hash = target; 
             });
     
             return false;
     });
 
-
-
-
 });
 
 
-
+// scroll to active state
 $(window).scroll(function() {
     var scrollDistance = $(window).scrollTop();
-
-    // Assign active class to nav links while scolling
     $('.page-section').each(function(i) {
             if ($(this).position().top <= scrollDistance) {
                 $('.menu_block a.active, nav a.active').removeClass('active');
@@ -54,7 +47,7 @@ $(window).scroll(function() {
 }).scroll();
 
 
-// Can also be used with $(document).ready()
+// flexslider2
 $(window).load(function() {
     $('#main-slider').flexslider({
         animation: "slide",
@@ -73,7 +66,6 @@ $(window).load(function() {
 
     });
 
-
     // lightbox 2
     lightbox.option({
         'resizeDuration': 200,
@@ -82,30 +74,25 @@ $(window).load(function() {
 
   });
 
+  (function () {
+      // modal
+      var modal = document.getElementById('myModal');
+      var btn = document.getElementById("more_items");
+      var span = document.getElementsByClassName("close")[0];
+      
+      btn.onclick = function() {
+          modal.style.display = "block";
+      }
+      
+      span.onclick = function() {
+          modal.style.display = "none";
+      }
+      
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              modal.style.display = "none";
+          }
+      }
+  }());
 
-    // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("more_items");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
